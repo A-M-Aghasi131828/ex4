0b6e96c411b3f940a42329bf9a8485424e9b1566
Arsham Aazami <arsham20302040aazami@gmail.com>
2022-12-03 13:53:31 +0330

    implementing knight killing in main.py

---
+++ bishop.py
@@ -0,0 +1,12 @@
+class Bishop():
+	def __init__(self, horizontal, vertical):
+		self.horizontal = horizontal
+		self.vertical = vertical
+
+	def kill_knight(self):
+		#state 1
+
+		#state 2
+		#state 3
+        #state 4
+		#state 5
---
+++ knight.py
@@ -0,0 +1,8 @@
+class Knight():
+	def __init__(self, horizontal, vertical):
+		self.horizontal = horizontal
+		self.vertical = vertical
+
+	def kill_bishop(self):
+
+
